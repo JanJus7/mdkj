50c77e36477010ad3693a131554d466f5b3d1f8e
JanJus7 <117662010+JanJus7@users.noreply.github.com>
2022-11-07 16:14:21 +0100

    Delete 5.py

--- 5.py
+++
@@ -1,6 +0,0 @@
-n = int(input("podaj wielkość jedengo trójkąta: "))
-for i in range(1, n + 1):
-    print("*" * i + " " * (n - i))
-print(" ")
-for i in range(1, n + 1):
-    print("*" * (n - i + 1) + " " * i)
