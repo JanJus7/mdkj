9579f31de5b32ad2ada50969fb27817ff0954d7d
JanJus7 <117662010+JanJus7@users.noreply.github.com>
2022-11-07 17:37:57 +0100

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,9 +1,5 @@
-print("Żeby znaleźć NWD -> ")
-a = int(input("Podaj większą liczbę: "))
-b = int(input("Podaj mniejszą liczbe: "))
-while a != b:
-    if a > b:
-        a = a - b
-    else:
-        b = b - a
-print(a)
+def przywitanie():
+    print("Hej!")
+
+
+przywitanie()
